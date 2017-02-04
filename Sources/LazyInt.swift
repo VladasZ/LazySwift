aac3e81a8fcb955f8a03841d652e0b82ca8d58de
Vladas Zakrevskis <146100@gmail.com>
2017-02-04 17:25:39 +0300

    LazyInt

---
+++ Sources/LazyInt.swift
@@ -0,0 +1,17 @@
+//
+//  LazyInt.swift
+//  LazySwift
+//
+//  Created by Vladas Zakrevskis on 2/4/17.
+//  Copyright © 2017 Vladas Zakrevskis. All rights reserved.
+//
+
+import UIKit
+
+public extension Int {
+    
+    var CGFloat:CGFloat {
+        
+        return CoreGraphics.CGFloat(self)
+    }
+}
