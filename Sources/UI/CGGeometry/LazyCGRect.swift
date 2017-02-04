204ad530239e4af2bc95d41677373db8d0990a53
Vladas Zakrevskis <146100@gmail.com>
2017-02-04 18:29:00 +0300

    new extensions

--- Sources/UI/CGGeometry/LazyCGRect.swift
+++ Sources/UI/CGGeometry/LazyCGRect.swift
@@ -10,6 +10,30 @@ import UIKit
 
 public extension CGRect {
     
+    var x:CGFloat {
+        
+        get { return origin.x }
+        set { origin.x = newValue }
+    }
+    
+    var y:CGFloat {
+        
+        get { return origin.y }
+        set { origin.y = newValue }
+    }
+    
+    var width:CGFloat {
+        
+        get { return size.width }
+        set { size.width = newValue }
+    }
+    
+    var height:CGFloat {
+        
+        get { return size.height }
+        set { size.height = newValue }
+    }
+    
     init(_ width:CGFloat, _ height:CGFloat) {
         
         self.init(x:0, y:0, width:width, height:height)
--- Sources/UI/Views/LazyUIView.swift
+++ Sources/UI/Views/LazyUIView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 public extension UIView {
     
+    //MARK: - Static methods
+    
     static let dummyTag = "dummy".hash
     
     static var dummy:UIView {
@@ -20,19 +22,19 @@ public extension UIView {
         return dummy
     }
     
+    //MARK: - Initializators
+
     convenience init(_ frame:CGRect) {
         
         self.init(frame:frame)
     }
     
-    func removeDummies() {
+    convenience init(_ x:CGFloat, _ y:CGFloat, _ width:CGFloat, _ height:CGFloat) {
         
-        for view in self.subviews {
-            if view.tag == UIView.dummyTag {
-                view.removeFromSuperview()
-            }
-        }
+        self.init(frame:CGRect(x, y, width, height))
     }
+    
+    //MARK: - Geometry
     
     var width:CGFloat {
         
@@ -43,7 +45,7 @@ public extension UIView {
         set {
             
             var frame = self.frame
-            frame.size.width = newValue
+            frame.width = newValue
             self.frame = frame
         }
     }
@@ -57,8 +59,65 @@ public extension UIView {
         set {
             
             var frame = self.frame
-            frame.size.height = newValue
+            frame.height = newValue
             self.frame = frame
+        }
+    }
+    
+    var x:CGFloat {
+        
+        get {
+            
+            return self.frame.origin.x
+        }
+        set {
+            
+            var frame = self.frame
+            frame.x = newValue
+            self.frame = frame
+        }
+    }
+    
+    var y:CGFloat {
+        
+        get {
+            
+            return self.frame.origin.y
+        }
+        set {
+            
+            var frame = self.frame
+            frame.y = newValue
+            self.frame = frame
+        }
+    }
+    
+    func set(width:CGFloat, height:CGFloat) {
+        
+        var frame = self.frame
+        frame.width = width
+        frame.height = height
+        self.frame = frame
+    }
+    
+    //MARK: - Positioning
+    
+    func stickToBottom() {
+        
+        if let superview = self.superview {
+            
+            self.y = superview.height - self.height
+        }
+    }
+    
+    //MARK: - Other
+    
+    func removeDummies() {
+        
+        for view in self.subviews {
+            if view.tag == UIView.dummyTag {
+                view.removeFromSuperview()
+            }
         }
     }
 }
