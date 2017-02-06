3ea018630793c5f14aa21fb8d69540b910bf7c33
Vladas Zakrevskis <146100@gmail.com>
2017-02-06 23:57:31 +0300

    viewNamed function

--- Sources/UI/Views/LazyUIView.swift
+++ Sources/UI/Views/LazyUIView.swift
@@ -166,3 +166,10 @@ public extension UIView {
         }
     }
 }
+
+//MARK: - Functions
+
+func viewNamed(_ nibName: String, bundle : Bundle? = nil) -> UIView? {
+    
+    return UINib(nibName: nibName, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as? UIView
+}
