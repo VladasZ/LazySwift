cea431f60cfba189fab999b39e1685cd763b701b
Vladas Zakrevskis <146100@gmail.com>
2017-02-08 10:33:48 +0300

    window changed to keyWindow

--- Sources/UI/Views/LazyViews.swift
+++ Sources/UI/Views/LazyViews.swift
@@ -14,7 +14,7 @@ func viewNamed(_ nibName: String, bundle : Bundle? = nil) -> UIView? {
     return UINib(nibName: nibName, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as? UIView
 }
 
-func window() -> UIView {
+public var keyWindow:UIView {
     
     return UIApplication.shared.keyWindow as! UIView
 }
