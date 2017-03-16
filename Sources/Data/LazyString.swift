b24db4dc093435b77a7b689bd86544bd8dedbe86
Vladas Zakrevskis <146100@gmail.com>
2017-03-17 00:48:04 +0300

    String randomWord and randomSentence properties

--- Sources/Data/LazyString.swift
+++ Sources/Data/LazyString.swift
@@ -10,7 +10,24 @@ import Foundation
 
 public extension String {
     
-    var firstLetter:String {
+    private static let letters = ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m",
+                                  "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    static var randomWord: String {
+        
+        var string = ""
+        for _ in 0...random(8) { string.append(letters[random(letters.count)]) }
+        return string
+    }
+    
+    static var randomSentence: String {
+        
+        var string = String.randomWord
+        for _ in 0...random(40) { string.append(" \(String.randomWord)") }
+        return string
+    }
+
+    var firstLetter: String {
         
         return String(characters.prefix(1))
     }
